05fcabee8ac04a5afdc48d052c9558dbf700d558
Monsjef <167148656+Monsjef@users.noreply.github.com>
2024-04-16 06:14:31 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,9 +21,7 @@ st.write('The name of the Smoothie will be: ', name_on_order)
 cnx=st.connection("snowflake")
 session=cnx.session()
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+
 #session = get_active_session()
 #my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
@@ -57,3 +55,8 @@ if ingredients_list:
      #session.sql(name_of_order).collect()
     
      st.success('Your Smoothie '+name_on_order+ ' is ordered!', icon="✅")
+
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response.json())
